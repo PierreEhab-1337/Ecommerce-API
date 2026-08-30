8c200a079b2ab6bae6649dd326925261879ab868
shaban <shabanaboelkair01@gmail.com>
2026-08-30 14:39:41 +0300

    order-model-task6

---
+++ src/models/Order.model.js
@@ -0,0 +1,85 @@
+const mongoose=require('mongoose')
+const orderSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    items:{
+        type:[orderItem],
+        required:true
+    },
+    shippingAddress:{
+        type:shippingAddress,
+        required:true
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['cash', 'stripe', 'paypal', 'paymob'],
+        default: 'cash',
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['pending', 'paid', 'failed', 'refunded'],
+        default: 'pending',
+    },
+    transactionId: {
+        type: String,
+    },
+    subtotal: {
+        type: Number,
+        required: true,
+    },
+    shippingFee: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    tax: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    discount: {
+        type: Number,
+        default: 0,
+    },
+
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ['pending','confirmed','processing','shipped','delivered','cancelled','returned',],
+        default: 'pending',
+    },
+
+    paidAt: {
+        type: Date,
+    },
+    deliveredAt: {
+        type: Date,
+    },
+    cancelledAt: {
+        type: Date,
+    },
+
+    customerNote: {
+        type: String,
+        maxLength: 1000,
+    },
+    adminNote: {
+        type: String,
+        maxLength: 1000,
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
+
