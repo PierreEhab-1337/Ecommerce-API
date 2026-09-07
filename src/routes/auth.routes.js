3657a36c4ca709b841c277bc701ac30c761eeac2
ehab <ehab@DESKTOP-6K3F9V9>
2026-09-07 04:13:01 +0300

    Minor changes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { login } from '../controllers/auth.controller.js';
-import { validate } from '../middleware/validateHandler.middleware.js';
+import validate from '../middleware/validateHandler.middleware.js';
 import { loginSchema } from '../validation/auth.validation.js';
 import asyncHandler from "../utils/asyncHandler.js"
 
