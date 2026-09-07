b489c3da8920d5c94ce9f46837e80d016759fef3
Pierre Ehab <108834103+PierreEhab-1337@users.noreply.github.com>
2026-09-07 03:42:12 +0300

    Wrap login handler with asyncHandler

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,14 +2,10 @@ import express from 'express';
 import { login } from '../controllers/auth.controller.js';
 import { validate } from '../middleware/validateHandler.middleware.js';
 import { loginSchema } from '../validation/auth.validation.js';
+import asyncHandler from "../utils/asyncHandler.js"
 
 const router = express.Router();
 
-router.post('/login', validate(loginSchema), login);
-
-import express from "express";
-import asyncHandler from "../utils/asyncHandler.js"
-
-;
+router.post('/login', validate(loginSchema), asyncHandler(login));
 
 export default router;
