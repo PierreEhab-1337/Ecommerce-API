f52e4d8a3e1e9444668b80dac498bba348c313e9
Rewan osama <rewan.ossama33@gmail.com>
2026-09-09 22:08:11 +0300

    feat: ForgetPass sendOTP and VerifyOTP

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,11 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.model.js';
 import createError from '../utils/createError.js';
 import asyncHandler from '../utils/asyncHandler.js';
+import crypto from "crypto";
+import OTP from "../models/OTP.model.js";
+import sendEmail from '../utils/sendEmail.js';
+import bcryptjs from "bcryptjs";
+
 
 export const login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -37,3 +42,69 @@ export const login = asyncHandler(async (req, res) => {
     data: { user: userData },
   });
 });
+
+//task3
+export const forgetPasswordSendOTP=asyncHandler(async(req, res)=>{
+const { email}= req.body;
+const user= await User.findOne({email});
+
+if(!user){
+  throw createError('User not found', 404);
+}
+const otp = crypto.randomInt(100000, 1000000).toString();
+
+const expiresAt = new Date(Date.now() + 5 * 60 * 1000);
+
+await OTP.create({
+  email,
+  otp,
+  expiresAt,
+  userData: {
+    username: user.username,
+    email: user.email
+  },
+});
+await sendEmail({
+  to: email,
+  subject: "Password reset OTP",
+  text: `Your OTP for password reset is ${otp}. It will expire in 5 minutes.`,
+  OTPNumber: otp,
+});
+res.status(200).json({
+  message: "OTP sent correctly",
+});
+});
+
+
+
+export const forgetPasswordVerifyOTP= asyncHandler( async(req, res)=>{
+  const {email, otp, newPassword}= req.body;
+  
+  const OTPRecord= await OTP.findOne({email}).sort({createdAt: -1});
+
+  if(!OTPRecord){
+    throw createError('Invalid OTP', 400);
+  }
+  if(OTPRecord.expiresAt< Date.now()){
+    throw createError('OTP has expired', 400);
+  }
+ const isValidOTP = await bcryptjs.compare(otp, OTPRecord.otp);
+ if(!isValidOTP){
+  throw createError('Invalid OTP', 400);
+ }
+ const user= await User.findOne({email});
+ if(!user){
+  throw createError('User not found', 404);
+ }
+
+ user.password = newPassword;
+ await user.save();
+
+ await OTP.findByIdAndDelete(OTPRecord._id);
+ res.status(200).json(
+  {
+    message: "Password reset successfully",
+    status: 'success'
+  })
+});
+////
