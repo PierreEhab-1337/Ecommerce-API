53b7f3fc6aff23024d47e626cad57894ba1079cf
mahmoud1amer2 <as0592489519@gmail.com>
2026-09-15 11:59:23 +0300

    updata on register data

--- src/validators/auth.validation.js
+++ src/validators/auth.validation.js
@@ -40,17 +40,12 @@ export const forgetPasswordSchema = Joi.object({
 
 
 export const verifyOTPSchema = Joi.object({
-  email: Joi.string().email().required().messages({
+  email: Joi.string().email().required().trim().messages({
     'string.email': "Please enter avalid email address",
     'any.required': "Email is required",
   }),
   otp: Joi.string().required().length(6).messages({
     'any.required': 'OTP is required'
   }),
-
-  newPassword: Joi.string().min(8).required().messages({
-    'string.min': 'Password must be at least 8 characters',
-    'any.required': 'Password is required',
-  })
 
 });
