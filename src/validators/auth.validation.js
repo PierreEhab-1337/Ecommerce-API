1c90ede9f6f7b2e55334d72eb6602c67bbf0566a
Pierre Ehab <108834103+PierreEhab-1337@users.noreply.github.com>
2026-09-15 15:46:04 +0300

    Rename OTP verification schema
    Renamed OTP schema to differentiate between registration and forget-password

--- src/validators/auth.validation.js
+++ src/validators/auth.validation.js
@@ -39,7 +39,7 @@ export const forgetPasswordSchema = Joi.object({
 });
 
 
-export const verifyOTPSchema = Joi.object({
+export const registerVerifyOTPSchema = Joi.object({
   email: Joi.string().email().required().trim().messages({
     'string.email': "Please enter avalid email address",
     'any.required': "Email is required",
@@ -47,5 +47,21 @@ export const verifyOTPSchema = Joi.object({
   otp: Joi.string().required().length(6).messages({
     'any.required': 'OTP is required'
   }),
+
+});
+
+export const forgetPasswordVerifyOTPSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'string.email': "Please enter avalid email address",
+    'any.required': "Email is required",
+  }),
+  otp: Joi.string().required().length(6).messages({
+    'any.required': 'OTP is required'
+  }),
+
+  newPassword: Joi.string().min(8).required().messages({
+    'string.min': 'Password must be at least 8 characters',
+    'any.required': 'Password is required',
+  })
 
 });
