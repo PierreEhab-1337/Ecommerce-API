39287864d53d26f6da325f98c4b0e3206d6ab56f
logain <logain0561@gmail.com>
2026-09-16 19:13:45 +0300

    feat: add orders pagination and status validation

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,9 +1,18 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
+import validate from "../middleware/validateHandler.middleware.js";
 import { getMyOrders } from "../controllers/order.controller.js";
+import { getMyOrdersSchema } from "../validators/order.validation.js";
 
 const router = express.Router();
 
-router.get("/my", authMiddleware, getMyOrders);
+router.get(
+  "/my",
+  authMiddleware,
+  validate(getMyOrdersSchema, "query"),
+  getMyOrders
+);
 
 export default router;
+
+
