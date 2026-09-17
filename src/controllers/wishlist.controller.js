6bb08db54494ff4e38a6545d5e929cb9821d9ba2
ehab <ehab@DESKTOP-6K3F9V9>
2026-09-17 19:30:35 +0300

    Pushed wishlist to user document

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -1,6 +1,7 @@
 import Wishlist from "../models/Wishlist.model.js";
 import createError from "../utils/createError.js";
 import Product from "../models/Product.model.js";
+import User from "../models/User.model.js";
 
 export const getWishlist= async (req, res)=>{
     const userId= req.user.id;
@@ -21,6 +22,11 @@ export const addToWishlist = async (req, res) => {
     const { productId } = req.params;
     const product = await Product.findById(productId);
 
+    const user = await User.findOne({_id: userId});
+
+    if(!user)
+        throw createError("User not found", 404);
+
     if (!product) {
         throw createError("Product not found", 404);
     }
@@ -38,22 +44,32 @@ export const addToWishlist = async (req, res) => {
             message: "Product added to wishlist"
         });
     }
-    if (wishlist.products.includes(productId)) {
+
+    if (wishlist.products.some((id) => id.equals(productId))) {
         throw createError("Product already in wishlist", 400);
     }
+
     wishlist.products.push(productId);
- await wishlist.save();
+    await (await wishlist.save()).populate("products");
+
+    user.wishlist.push(productId);
+    await user.save();
 
     res.status(200).json({
         success: true,
-        message: "Product added to wishlist"
+        message: "Product added to wishlist",
+        data: wishlist,
     });
-    console.log("wishlist after adding product:", wishlist);
 };
 
 export const removeFromWishlist = async (req, res) => {
     const userId= req.user.id;
     const {productId}= req.params;
+
+    const user = await User.findOne({_id: userId});
+
+    if(!user)
+        throw createError("User not found", 404);
 
     const wishlist = await Wishlist.findOne({ user: userId });
     if(!wishlist)
@@ -65,13 +81,17 @@ export const removeFromWishlist = async (req, res) => {
         throw createError("Product not in wishlist", 400);
     }
     
-        wishlist.products.remove(productId);
-        await wishlist.save();
+    wishlist.products.remove(productId);
+    await wishlist.save();
 
-        return res.status(200).json({
-            success: true,
-            message: "Product removed from wishlist"
-        });
-    }
+    user.wishlist = user.wishlist.filter((id) => (
+        !id.equals(productId)
+    ));
+    await user.save();
 
-
+    return res.status(200).json({
+        success: true,
+        message: "Product removed from wishlist",
+        data: wishlist,
+    });
+}
