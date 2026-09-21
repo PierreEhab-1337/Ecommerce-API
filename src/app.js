a8cb8f9681a98e394a57586e49aaf953c9837254
ehab <ehab@DESKTOP-6K3F9V9>
2026-09-21 21:50:21 +0300

    swagger setup update

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ import adminRouter from "./routes/admin.routes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./docs/swagger.json" with { type: "json" };
 
+const CDN = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14";
+
 const app = express();
 
 app.use(cors());
@@ -24,7 +26,19 @@ app.use('/stripe/webhook', stripeRouter)
 //Parses incoming JSON data sent from the frontend
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customCssUrl: `${CDN}/swagger-ui.min.css`,
+    customJs: [
+      `${CDN}/swagger-ui-bundle.min.js`,
+      `${CDN}/swagger-ui-standalone-preset.min.js`,
+    ],
+  })
+);
+
 app.use(cookieParser());
 
 
